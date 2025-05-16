704b743ebdf3d1f236167cebd498aa32c2ab8dba
Paras Agarwal <parasagarwal926@gmail.com>
2025-05-16 22:40:43 +0530

    Changes to Heap_01/Calloc

--- Heap/Calloc.c
+++ Heap/Calloc.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    
+int main()
+{
+
     // Use of malloc
     // Malloc allocates the requested amount of memory malloc(n*sizeof())
-    int* ptr;
-    int i;
-    
-    ptr = (int *)malloc(3*sizeof(int));
-    for(i=0;i<3;i++) {
+    int *ptr;
+    int n, i;
+
+    printf("Enter the size of array: \n");
+    scanf("%d", &n);
+    ptr = (int *)malloc(3 * sizeof(int));
+    for (i = 0; i < n; i++)
+    {
         printf("Enter the value no %d of this array: \n", i);
         scanf("%d", &ptr[i]);
     }
-    for(i=0;i<3;i++) {
+    for (i = 0; i < n; i++)
+    {
         printf("The value at no %d of this array is: %d\n", i, ptr[i]);
     }
-    
-    
+
     return 0;
 }
