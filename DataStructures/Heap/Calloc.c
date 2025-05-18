d5dcc774e93d05741d3c717e97c86a2f96584468
Paras Agarwal <parasagarwal926@gmail.com>
2025-05-18 15:34:31 +0530

    Programs for Heap

---
+++ DataStructures/Heap/Calloc.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    // Use of Calloc
+    // Calloc allocates the requested amount of memory malloc(n, sizeof())
+    // It assigns value 0 if no input is provided
+    int *ptr;
+    int n, i;
+
+    printf("Enter the size of array: \n");
+    scanf("%d", &n);
+    ptr = (int *)calloc(n, sizeof(int));
+    for (i = 0; i < n; i++)
+    {
+        printf("Enter the value no %d of this array: \n", i);
+        scanf("%d", &ptr[i]);
+    }
+    for (i = 0; i < n; i++)
+    {
+        printf("The value at no %d of this array is: %d\n", i, ptr[i]);
+    }
+
+    return 0;
+}
---
+++ DataStructures/Heap/Malloc.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    // Use of Malloc
+    // Malloc allocates the requested amount of memory malloc(n*sizeof())
+    int *ptr;
+    int n, i;
+
+    printf("Enter the size of array: \n");
+    scanf("%d", &n);
+    ptr = (int *)malloc(3 * sizeof(int));
+    for (i = 0; i < n; i++)
+    {
+        printf("Enter the value no %d of this array: \n", i);
+        scanf("%d", &ptr[i]);
+    }
+    for (i = 0; i < n; i++)
+    {
+        printf("The value at no %d of this array is: %d\n", i, ptr[i]);
+    }
+
+    return 0;
+}
