902eb0905c54d34f8fcb10bb2a5a9879d2ff899d
parasag07 <parasagarwal926@gmail.com>
2025-10-01 12:36:53 +0530

    Updated Bubble Sort

--- Algorithms/Sorting/BubbleSort.cpp
+++ Algorithms/Sorting/BubbleSort.cpp
@@ -15,7 +15,7 @@ void traversal(int arr[], int size){
 
 void BubbleSort(int arr[], int size){
     for(int i=0; i<size-1; i++){
-        for(int j = 1; j<size; j++){
+        for(int j = 1; j<size-i; j++){
             if(arr[j-1]>arr[j]){
                 int temp = arr[j];
                 arr[j] = arr[j-1];
